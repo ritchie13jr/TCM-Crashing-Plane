fcbe08008d23a50c2d9130e809125190fcc09c8d
ritchie13jr <xritchie13@gmail.com>
2024-04-09 16:24:21 +0200

    Try to trigger

---
+++ Assets/Scripts/Star Collector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    pulbic int points;
+
+    void Start() 
+    {
+        this.points = 0;
+    }
+
+    void Update(gameObject other)
+    {
+        if () 
+        {
+            this.points++;
+            other.Destroy;
+        }
+    }
+}
